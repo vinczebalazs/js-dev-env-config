6ba3db65e2bc84382d39bfd5e91f1f0b9cc23f1a
Balazs Vincze <sayhello@bvincze.com>
2018-08-24 15:32:13 +0200

    Fixed 'consistent-return' rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'consistent-return': ['error', 'always'],
+    'consistent-return': 'error',
     'func-names': ['off', 'as-needed'],
   },
   parserOptions: {
